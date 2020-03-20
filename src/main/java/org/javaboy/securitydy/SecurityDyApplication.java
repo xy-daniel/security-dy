f81726736e846101382a610ce8d247f1c22ea889
damiel <daniel@users.noreply.github.com>
2020-03-20 16:21:04 +0800

    pom、配置文件初始化

---
+++ src/main/java/org/javaboy/securitydy/SecurityDyApplication.java
@@ -0,0 +1,13 @@
+package org.javaboy.securitydy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurityDyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SecurityDyApplication.class, args);
+    }
+
+}
